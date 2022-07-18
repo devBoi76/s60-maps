4daa03eaa48df6117e7da29e16bfba926df83146
devBoi76 <mrzyglodjan@protonmail.com>
2022-07-18 14:27:46 +0200

    misc

--- inc/ApiKeys.h
+++ inc/ApiKeys.h
@@ -11,9 +11,9 @@
 // API Keys
 // You can get your own here: https://manage.thunderforest.com/
 #ifdef _DEBUG
-_LIT8(KThunderForestApiKey, ""); // Development key
+_LIT8(KThunderForestApiKey, "4c0fb669f9c6418897d0289faf43ecb0"); // Development key
 #else
-_LIT8(KThunderForestApiKey, ""); // Production key
+_LIT8(KThunderForestApiKey, "4c0fb669f9c6418897d0289faf43ecb0"); // Production key
 #endif
 
 #endif /* APIKEYS_H_ */
